c47549dc42a16a2d372e2ff77833464fa9043d1c
Anjitha Mohanan <anjithamohanan263@gmail.com>
2023-04-16 22:24:51 +0530

    first commit

---
+++ frontend/src/pages/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import ChatBox from '../components/ChatBox'
+
+export default function HomePage() {
+  return <div>
+    <div >
+       <h1> Welcome </h1>
+        </div>
+
+    <ChatBox />  
+    </div>
+}
